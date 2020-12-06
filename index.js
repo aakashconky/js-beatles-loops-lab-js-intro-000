7bf70dae686840399f57363c913ced00f5a5fcee
aakash thapa <aakash_conky@hotmail.com>
2020-12-06 02:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ funtion theBeatlesPlay ( musicians , instruments) ;
 
 cost arr = []
 
-  for (let i = 0, l = musicians.length; i < l; i++) {	
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)	
-  }	
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 
-  return arr	
+  return arr
 }
 
-function johnLennonFacts(facts) {	
-  const shoutedFacts = []	
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
 
-  let i = 0	
-  while (i < facts.length) {	
-    shoutedFacts.push(`${facts[i]}!!!`)	
-    i++	
-  }	
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
 
-  return shoutedFacts	
-}	
+  return shoutedFacts
+}
 
-function iLoveTheBeatles(n) {	
-  const arr = []	
+function iLoveTheBeatles(n) {
+  const arr = []
 
-  do {	
-    arr.push('I love the Beatles!')	
-    n++	
-  } while (n < 15)	
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
 
-  return arr	
+  return arr
 }
