05f390a5bad7538a01af4e83ea3d852edc044f9b
aakash thapa <aakash_conky@hotmail.com>
2020-12-06 02:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ return musicianInstrument;
 
 
 const facts=[
-“He was the last Beatle to learn to drive”,
-“He was never a vegetarian”,
-“He was a choir boy and boy scout”,
-“He hated the sound of his own voice”
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
 ];
 function johnLennonFacts(factsArray){
 var i=0;
 var newArray=[];
 while (factsArray.length > i){
-newArray.push(`${factsArray[i]}` + “!!!”);
+newArray.push(`${factsArray[i]}` + "!!!");
 i++;
 }
 return newArray;
@@ -34,3 +34,5 @@ strings.push(‘I love the Beatles!’)
 }while (n < 15);
 return strings;
 }
+
+ 
